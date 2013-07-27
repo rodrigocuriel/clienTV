bc4d6b148dcde0b838d497871e2df367729b6030
Rodrigo Curiel <rodrigo.salazar@onstar.com>
2013-07-26 22:35:35 -0400

    updating install file

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@ if [ ! -d ~/.clienTV ]
 then
   echo "\033[0;33mYou don't have clienTV installed.\033[0m I'll try to download it\033[0m"
 
-  if [ ! -d ~/.clienTV ]
+  if [ ! -d ~/movies ]
   then
     echo "\033[0;33mcreating directories\033[0m"
     mkdir ~/movies
   fi
-  
+
   echo "\033[0;33minstalling curl\033[0m"
   sudo apt-get install curl
 
@@ -26,7 +26,7 @@ cd ~/.clienTV
 git pull
 
 echo "\033[0;34mUsing the clienTV template file and adding it to ~/.xbmc/userdata\033[0m"
-ln -sf ~/.xbmc/userdata/autoexec.py ~/.clienTV/autoexec.py
+ln -sf  ~/.clienTV/autoexec.py ~/.xbmc/userdata/autoexec.py
 # cp ~/.clienTV/autoexec.py ~/.xbmc/userdata/
 
 #echo "\033[0;34mCopying your current PATH and adding it to the end of ~/.zshrc for you.\033[0m"
