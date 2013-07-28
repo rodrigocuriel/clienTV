b70c3531364dbd70182abce644367d8f21f3354f
Rodrigo Curiel <rodrigo.salazar@onstar.com>
2013-07-27 21:16:34 -0400

    update

--- install.sh
+++ install.sh
@@ -39,8 +39,6 @@ then
   echo "\033[0;33mcreating directories\033[0m"
   mkdir ~/movies
   cd ~/movies
-  wget echo "\033[0;33mdownloading sample playlist\033[0m"
-  wget https://www.dropbox.com/sh/aca6ga51qkflqmr/dg65XuuxYw/playlist001.rtf
 fi
 
 echo "\033[0;33mDownloading latest version\033[0m"
